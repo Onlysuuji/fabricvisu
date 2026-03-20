e70da043634047e26e393fec979c1ed6577e8127
onlysuuji <onlysuuji@outlook.jp>
2026-03-21 03:06:55 +0900

    first commit

---
+++ src/client/java/org/example2/visualizer/client/VisualizerClient.java
@@ -0,0 +1,10 @@
+package org.example2.visualizer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VisualizerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/example2/visualizer/Visualizer.java
@@ -0,0 +1,10 @@
+package org.example2.visualizer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Visualizer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
